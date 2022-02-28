664f24df8f7e40661996dde1f77889f1b5a7b90d
KojiroSchatten <banbirou@icloud.com>
2022-03-01 00:08:46 +0900

    add: schema file

--- db/migrate/20220228123516_create_line_foods.rb
+++ db/migrate/20220228123516_create_line_foods.rb
@@ -1,7 +1,11 @@
 class CreateLineFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :line_foods do |t|
-
+      t.references :food, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
+      t.references :order, foreign_key: true #order確定まで、line_foodsレコードは、order知らなくても良いため
+      t.integer :count, null: false, default: 0
+      t.boolean :active, null: false, default: false
       t.timestamps
     end
   end
--- db/migrate/20220228123534_create_orders.rb
+++ db/migrate/20220228123534_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-
+      t.integer :total_price, null: false, default: 0
       t.timestamps
     end
   end
