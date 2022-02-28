e1f32bf49ecbb3a2809ccfdbc1c99d946f3df814
KojiroSchatten <banbirou@icloud.com>
2022-03-01 00:32:51 +0900

    modify: routes and readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 restaurants 1 : N foods
 foods       1 : 1 line_foods
 line_foods  1 : 1 orders
+
+# 出力されたrouts一覧から、'order'が含まれる一行のみを出力する
+$ rails routes | grep order
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # namespace -> コントローラーをグルーピングし、またURLにもその情報を付与する
+  namespace :api do
+    namespace :v1 do
+      # 特定のリソースに対するルーティングはresourcesを使う
+      # resources :foods, だけだと、7つのHTTPメソッド作られるので注意
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create]
+      # 'line_foods/replace' というPUTリクエスト -> line_foods_controller.rbのreplaceメソッド
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
